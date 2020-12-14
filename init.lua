68ebead6fa0eae34d04fc258e43f2a2959b54066
SenselessDemon <69490453+senselessdemon@users.noreply.github.com>
2020-12-13 20:08:14 -0800

    Updated default batch-key

--- init.lua
+++ init.lua
@@ -5126,7 +5126,7 @@ function Parser.new(options)
 	local self = setmetatable({
 		options = options or {
 			prefix = "/",
-			batchKey = "|",
+			batchKey = ";",
 			splitKey = " ",
 			overrideKey = "\"",
 			coreArgumentPrefix = "--",
